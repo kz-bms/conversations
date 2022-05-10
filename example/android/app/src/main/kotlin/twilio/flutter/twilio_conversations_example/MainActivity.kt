4a2eaaba214f279e80e85d6a0a0050763ca23557
Shreyas S <shreyassgowda97@gmail.com>
2022-05-10 15:10:24 +0530

    Removed example folder.

--- example/android/app/src/main/kotlin/twilio/flutter/twilio_conversations_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package twilio.flutter.twilio_conversations_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- example/lib/util.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class ConversationsUtil {
-  static String parseDateTime(DateTime timestamp,
-      {String format = 'MMM d, h:mm a'}) {
-    final dateTime = timestamp.toLocal();
-    return DateFormat(format).format(dateTime).toString();
-  }
-}
